e576ff700b16021c74f5b69bb8759fbad29a1fdf
yiikaii <esmondpeh@gmail.com>
2015-02-19 12:48:00 +0800

    add DatabaseController.java for storage, includes JSON jar for development

---
+++ src/DatabaseController.java
@@ -0,0 +1,23 @@
+/*
+ * @author: Esmond Peh
+ * 
+ * This controller is to provide the read and write functionalities 
+ * to the storage for the LogicController.
+ * 
+ * JSON files:
+ * 1. 	activeTaskList.json
+ * 2.	archiveTaskList.json
+ * 3.	dateList.json
+ * 4.	priorityList.json
+ * 5.	tagList.json
+ * 
+ */
+
+public class DatabaseController {
+	
+	
+	
+	public static void main (String[] args) {
+		
+	}
+}
