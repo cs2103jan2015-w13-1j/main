f54a8efa50d0cd6aeeb5cdd8735701ca09ee7241
ks-uppercut <tankangsoon@gmail.com>
2015-02-22 20:23:13 +0800

    Added table with scrollbar. Changed to a tab interface.

--- src/javafxTest/javafxTest.java
+++ src/javafxTest/javafxTest.java
@@ -1,11 +1,17 @@
 package javafxTest;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -31,51 +37,40 @@ public class javafxTest extends Application{
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25,25,25,25));
 		
-		Text scenetitle = new Text("HeyBuddy!");
-		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
-		grid.add(scenetitle, 0, 0);
+		Text sceneTitle = new Text("HeyBuddy!");
+		sceneTitle.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 50));
+		sceneTitle.setFill(Color.WHITE);
+		grid.add(sceneTitle, 0, 0);
 		
 		//mid section-------------------------------
 		Group mid = new Group();
 		
 		Group taskPanel = new Group();
-        Rectangle rect1 = new Rectangle(60, 100, 750, 400);
-        rect1.setFill(Color.WHITE);
-        rect1.setStroke(Color.web("#00687F"));
-        rect1.setStrokeWidth(10);
-        rect1.setArcHeight(50);
-        rect1.setArcWidth(50);
-        Text taskTitle = new Text("Tasks");
-        taskTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
-        taskTitle.setX(rect1.getX() + 40);
-        taskTitle.setY(rect1.getY() + 35);
-        taskPanel.getChildren().addAll(rect1, taskTitle);
+        createPanel(taskPanel, 0, "Tasks", "49CEFF");
+        
+        //initialise and add task table here
+        TableView taskTable = new TableView();
+        TableColumn taskList = new TableColumn("Tasks");
+        TableColumn archive = new TableColumn("Archive");
+        TableColumn id = new TableColumn("#");
+        TableColumn description = new TableColumn("Description");
+        TableColumn date = new TableColumn("Date");
+        taskList.getColumns().addAll(id, description, date);
+        archive.getColumns().addAll(id, description);
+        taskTable.getColumns().addAll(taskList, archive);
+        taskTable.setMaxHeight(360);
+        taskTable.setTranslateX(30);
+        taskTable.setTranslateY(70);
+ 
+        taskPanel.getChildren().addAll(taskTable);
+        //create an observable array list
+        
          
         Group helpPanel = new Group();
-        Rectangle rect2 = new Rectangle(30, 50, 750, 400);
-        rect2.setFill(Color.WHITE);
-        rect2.setStroke(Color.web("#009CBF"));
-        rect2.setStrokeWidth(10);
-        rect2.setArcHeight(50);
-        rect2.setArcWidth(50);
-        Text helpTitle = new Text("Commands");
-        helpTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
-        helpTitle.setX(rect2.getX() + 40);
-        helpTitle.setY(rect2.getY() + 35);
-        helpPanel.getChildren().addAll(rect2, helpTitle);
+        createPanel(helpPanel, 300, "Commands", "41BAE5");
                 
         Group settingPanel = new Group();
-        Rectangle rect3 = new Rectangle(0, 0, 750, 400);
-        rect3.setFill(Color.WHITE);
-        rect3.setStroke(Color.web("#00D0FF"));
-        rect3.setStrokeWidth(10);
-        rect3.setArcHeight(50);
-        rect3.setArcWidth(50);
-        Text settingTitle = new Text("Settings");
-        settingTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
-        settingTitle.setX(rect3.getX() + 40);
-        settingTitle.setY(rect3.getY() + 35);
-        settingPanel.getChildren().addAll(rect3, settingTitle);
+        createPanel(settingPanel, 600, "Settings", "369BBF");
          
         mid.getChildren().addAll(settingPanel, helpPanel, taskPanel);
         
@@ -90,15 +85,17 @@ public class javafxTest extends Application{
 		grid.add(commandPane, 0, 2);
 		
 		Text command = new Text("Command: ");
-		command.setFont(Font.font("Tahmoma", FontWeight.NORMAL, 20));
+		command.setFont(Font.font("Tahmoma", FontWeight.BOLD, 25));
+		command.setFill(Color.WHITE);
 		commandPane.add(command, 0, 0);
 		
 		Text actionTarget = new Text();
-		actionTarget.setFont(Font.font("Tahmoma", FontWeight.NORMAL, 20));
+		actionTarget.setFont(Font.font("Tahmoma", FontWeight.BOLD, 25));
+		actionTarget.setFill(Color.web("123440"));
 		commandPane.add(actionTarget, 1, 2);
 				
 		TextField commandTextField = new TextField();
-		commandTextField.setPrefWidth(700); 
+		commandTextField.setPrefWidth(760); 
 		commandPane.add(commandTextField, 1, 0);
 		commandTextField.setOnAction(new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent e){
@@ -138,10 +135,27 @@ public class javafxTest extends Application{
 		
 		//------------------------------------
 		
-		Scene scene = new Scene(grid, 900, 700);
+		Scene scene = new Scene(grid, 950, 700);
 		stage.setScene(scene);
 		scene.getStylesheets().add(javafxTest.class.getResource("style.css").toExternalForm());
 		stage.show();
+	}
+
+	private void createPanel(Group panel, int x, String title, String color) {
+		Rectangle tab = new Rectangle(x, 0, 300, 80);
+		tab.setFill(Color.web(color));
+        tab.setArcHeight(30);
+        tab.setArcWidth(30);
+		Rectangle rect = new Rectangle(0, 50, 900, 400);
+        rect.setFill(Color.web(color));
+        rect.setArcHeight(50);
+        rect.setArcWidth(50);
+        Text panelTitle = new Text(title);
+        panelTitle.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 35));
+        panelTitle.setFill(Color.web("123440"));
+        panelTitle.setX(tab.getX() + 40);
+        panelTitle.setY(tab.getY() + 40);
+        panel.getChildren().addAll(rect, tab, panelTitle);
 	}
 
 }
