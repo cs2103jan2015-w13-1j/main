c9cec7a2d44828bfd0a4b1dd4b10224946be66e2
Zhou Yichen <zhouyichen123@gmail.com>
2015-02-25 14:11:05 +0800

    Complete initialise function in Logic

--- src/LogicController.java
+++ src/LogicController.java
@@ -6,8 +6,31 @@ public class LogicController implements InterfaceForLogic{
 	
 	private StorageController DC = new StorageController();
 	private DATA data;
-	public DATA initialise() {
-		return DC.getAllData();
+	
+	//The hashMaps
+	private TaskList activeTaskList;
+	private TaskList archivedTaskList;
+	private TagList tagList;
+	private DateList dateList;
+	private PriorityList priorityList;
+	
+	//The treeSets
+	private ArchiveSortedList archiveSortedList;
+	private PrioritySortedList prioritySortedList;
+	private TodoSortedList toDoSortedList;
+	
+	public void initialise() {
+		data = DC.getAllData();
+		
+		activeTaskList = data.getActiveTaskList();
+		archivedTaskList = data.getArchivedTaskList();
+		tagList = data.getTagList();
+		dateList = data.getDateList();
+		priorityList = data.getPriorityList();
+		
+		archiveSortedList = data.getArchiveSortedList();
+		prioritySortedList = data.getPrioritySortedList();
+		toDoSortedList = data.getToDoSortedList();
 	}
 	
 	public String addTask(Task task) {
