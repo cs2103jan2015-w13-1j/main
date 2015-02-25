a419610b6df96541f5d28257be86cad8a0e20cc0
Hanif <hahahanif@gmail.com>
2015-02-25 14:51:59 +0800

    Updated InterfaceForLogic with required functions

--- src/InterfaceForLogic.java
+++ src/InterfaceForLogic.java
@@ -1,8 +1,16 @@
 import basicElements.*;
-
+import treeSets.*;
 
 public interface InterfaceForLogic {
-	String addTask(Task task);
-	
-	
+	TodoSortedList addTask(Task task); //sorted by date
+	TodoSortedList editStartDate(Task task, Date start);
+	TodoSortedList editDeadLine(Task task, Date deadLine);
+	TodoSortedList addDeadLine(Task task, Date deadLine);
+	GeneralSortedList searchByDate(Date searchDate);
+	TaskByTag searchByTag(String tag);	//not sure of return type and input type
+	PrioritySortedList searchByPriority(); //has no priority object? how to store priority?
+	ArchiveSortedList moveToArchive(Task task); //not sure of the return type
+	ArchiveSortedList viewArchiveTasks();
+	TodoSortedList deleteTask(Task task);
+	TodoSortedList modifyTask(Task task);//not sure of input type
 }
