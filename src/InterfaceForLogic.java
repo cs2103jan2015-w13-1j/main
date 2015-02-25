d42108a5ce3ceda8f263ef882a93612c01a27b15
Hanif <hahahanif@gmail.com>
2015-02-25 16:09:30 +0800

    Edited return type for search functions to ArrayList<Task>

--- src/InterfaceForLogic.java
+++ src/InterfaceForLogic.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
 import basicElements.*;
 import treeSets.*;
 
 public interface InterfaceForLogic {
+	int getSerialNumber();
 	TodoSortedList addTask(Task task); //sorted by date
 	TodoSortedList editStartDate(Task task, Date start);
 	TodoSortedList editDeadLine(Task task, Date deadLine);
 	TodoSortedList addDeadLine(Task task, Date deadLine);
-	GeneralSortedList searchByDate(Date searchDate);
-	TaskByTag searchByTag(String tag);	//not sure of return type and input type
-	PrioritySortedList searchByPriority(); //has no priority object? how to store priority?
+	ArrayList<Task> searchByDate(Date searchDate);
+	ArrayList<Task> searchByTag(String tag);	//not sure of return type and input type
+	ArrayList<Task> searchByPriority(); //has no priority object? how to store priority?
 	ArchiveSortedList moveToArchive(Task task); //not sure of the return type
 	ArchiveSortedList viewArchiveTasks();
 	TodoSortedList deleteTask(Task task);
