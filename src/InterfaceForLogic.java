0a4fc8f26a5eee0c3da065d462d880717a6db12c
Hanif <hahahanif@gmail.com>
2015-02-25 16:20:30 +0800

    fixed types and class names in InterfaceForLogic and ToDoSortedList from merge conflict issues

--- src/InterfaceForLogic.java
+++ src/InterfaceForLogic.java
@@ -3,24 +3,18 @@ import basicElements.*;
 import treeSets.*;
 
 public interface InterfaceForLogic {
-<<<<<<< HEAD
+
 	int getSerialNumber();
-	TodoSortedList addTask(Task task); //sorted by date
-	TodoSortedList editStartDate(Task task, Date start);
-	TodoSortedList editDeadLine(Task task, Date deadLine);
-	TodoSortedList addDeadLine(Task task, Date deadLine);
-	ArrayList<Task> searchByDate(Date searchDate);
-	ArrayList<Task> searchByTag(String tag);	//not sure of return type and input type
-	ArrayList<Task> searchByPriority(); //has no priority object? how to store priority?
-=======
 	ToDoSortedList addTask(Task task); //sorted by date
 	ToDoSortedList editStartDate(Task task, Date start);
 	ToDoSortedList editDeadLine(Task task, Date deadLine);
 	ToDoSortedList addDeadLine(Task task, Date deadLine);
-	GeneralSortedList searchByDate(Date searchDate);
-	TaskByTag searchByTag(String tag);	//not sure of return type and input type
-	PrioritySortedList searchByPriority(); //has no priority object? how to store priority?
->>>>>>> c6517ad09beb1179d61d65e90408dfb21f3c3556
+	ArrayList<Task> searchByDate(Date searchDate);
+	ArrayList<Task> searchByTag(String tag);	//not sure of return type and input type
+	ArrayList<Task> searchByPriority(); //has no priority object? how to store priority?
+
+
+
 	ArchiveSortedList moveToArchive(Task task); //not sure of the return type
 	ArchiveSortedList viewArchiveTasks();
 	ToDoSortedList deleteTask(Task task);
