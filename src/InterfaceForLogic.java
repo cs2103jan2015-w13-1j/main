c6517ad09beb1179d61d65e90408dfb21f3c3556
Zhou Yichen <zhouyichen123@gmail.com>
2015-02-25 16:01:47 +0800

    Fixed Typo for "TodoSortedList"

--- src/InterfaceForLogic.java
+++ src/InterfaceForLogic.java
@@ -2,15 +2,15 @@ import basicElements.*;
 import treeSets.*;
 
 public interface InterfaceForLogic {
-	TodoSortedList addTask(Task task); //sorted by date
-	TodoSortedList editStartDate(Task task, Date start);
-	TodoSortedList editDeadLine(Task task, Date deadLine);
-	TodoSortedList addDeadLine(Task task, Date deadLine);
+	ToDoSortedList addTask(Task task); //sorted by date
+	ToDoSortedList editStartDate(Task task, Date start);
+	ToDoSortedList editDeadLine(Task task, Date deadLine);
+	ToDoSortedList addDeadLine(Task task, Date deadLine);
 	GeneralSortedList searchByDate(Date searchDate);
 	TaskByTag searchByTag(String tag);	//not sure of return type and input type
 	PrioritySortedList searchByPriority(); //has no priority object? how to store priority?
 	ArchiveSortedList moveToArchive(Task task); //not sure of the return type
 	ArchiveSortedList viewArchiveTasks();
-	TodoSortedList deleteTask(Task task);
-	TodoSortedList modifyTask(Task task);//not sure of input type
+	ToDoSortedList deleteTask(Task task);
+	ToDoSortedList modifyTask(Task task);//not sure of input type
 }
