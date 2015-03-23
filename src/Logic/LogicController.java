f29ab0dcc5ba9381c4ce1e41666f353ca05b5688
zhouyichen <zhouyichen123@gmail.com>
2015-03-23 17:43:35 +0800

    Modified add recurrence task method to accept long instead of Date as period

--- src/Logic/LogicController.java
+++ src/Logic/LogicController.java
@@ -288,7 +288,7 @@ public class LogicController implements InterfaceForLogic{
 	}
 
 	@Override
-	public ToDoSortedList addRecurringTask(Task task, Date period, int recurrenceNum) {
+	public ToDoSortedList addRecurringTask(Task task, long period, int recurrenceNum) {
 		assert(!task.getType().equals("generic"));
 		historyController.log();
 		
@@ -297,7 +297,7 @@ public class LogicController implements InterfaceForLogic{
 		task.setRecurrenceId(recurrenceId);
 		activeTaskList.addTask(task.getId(), task);
 		for (int i = 1; i < recurrenceNum ; i++) {
-			Task newTask = task.copyWithInterval(id + i, i * period.getTime());
+			Task newTask = task.copyWithInterval(id + i, i * period);
 			newTask.setRecurrenceId(recurrenceId);
 			activeTaskList.addTask(id + i, newTask);
 		}
