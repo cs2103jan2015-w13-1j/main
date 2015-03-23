bc010516dfce3bdb3bec07c716b6446e8cd924d8
zhouyichen <zhouyichen123@gmail.com>
2015-03-23 17:41:12 +0800

    Add customised constructor to the Date object

--- src/Common/Date.java
+++ src/Common/Date.java
@@ -7,4 +7,23 @@ public class Date extends java.util.Date{
 		SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
 		return df.format(this);
 	}
+	
+	public Date(String interval) {
+		super();
+		switch(interval) {
+		case "oneHour":
+			this.setTime(3600000);
+			break;
+		case "oneDay":
+			this.setTime(86400000);
+			break;
+		case "oneWeek":
+			this.setTime(604800000);
+			break;
+		}
+	}
+	
+	public Date() {
+		super();
+	}
 }
