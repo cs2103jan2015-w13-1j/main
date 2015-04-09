62fe6a311875096412ecff6e7a9a294eee24e410
ks-uppercut <tankangsoon@gmail.com>
2015-04-09 12:19:53 +0800

    Change border design. GUI looks 100% prettier now.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
@@ -14,6 +15,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.initStyle(StageStyle.UNDECORATED);
 			primaryStage.show();
 
 			//primaryStage.close();
